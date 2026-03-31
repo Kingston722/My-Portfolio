1a804afa34933757a33cca054d90b9ae67ed5d22
Harshvardhan <harshvardhanwork01@gmail.com>
2026-03-31 22:09:14 +0530

    fix: pin circles asset to bottom-right

--- components/Circles.jsx
+++ components/Circles.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Circles = () => {
   return (
-    <div className="w-[200px] xl:w-[300px] absolute -right-16 -bottom-2 mix-blend-color-dodge animate-pulse duration-75 z-10">
+    <div className="w-[200px] xl:w-[300px] fixed right-0 bottom-0 translate-x-1/4 translate-y-1/4 mix-blend-color-dodge animate-pulse duration-75 z-10 pointer-events-none">
       <Image
         src="/circles.png"
         alt="circles"
